c1ee4879676e786bc70348d963004e1be45cbef6
syuu-22397 <66163698+syuu-22397@users.noreply.github.com>
2020-09-10 21:12:28 +0900

    Update sortriddle.py

--- sortriddle.py
+++ sortriddle.py
@@ -15,7 +15,7 @@ from discord.ext import commands
 # サーバのトークン公開しないこと
 json_open = open('./info.json')
 json_load = json.load(json_open)
-TOKEN = json.load['token']
+token = os.environ["DISCORD_BOT_TOKEN"]
 
 # コマンドプレフィックスの指定
 bot = commands.Bot(command_prefix='/')
